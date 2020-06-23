46d1b384bd5745601e9514afa7add570bd0e7f69
Ed Morris <ecm200@gmail.com>
2020-06-23 13:06:22 +0000

    Updates to feature maps extraction, all models completed
    Feature map extraction notebooks completed for all models trained.
    Placed universal functions into external module files.

---
+++ src/layer_feature_maps.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+
+def extract_feature_maps(model, dataloader, fc_feature_extractions, device):
+    was_training = model.training
+    model.eval()
+
+    img_paths = []
+    print('Commencing predictions and feature extraction minibatch..', end='')
+    with torch.no_grad():
+        for i, (inputs, labels, paths) in enumerate(dataloader):
+            if i % 25 == 0:
+                print('{}..'.format(i), end='')
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+
+            out = model(inputs)
+            _, preds = torch.max(out, 1)
+
+            if i == 0:
+                labels_truth = labels.cpu().numpy()
+                labels_pred = preds.cpu().numpy()
+                for path in paths:
+                    img_paths.append(path)
+            else:
+                labels_truth = np.concatenate((labels_truth,labels.cpu().numpy()))
+                labels_pred = np.concatenate((labels_pred,preds.cpu().numpy()))
+                for path in paths:
+                    img_paths.append(path)
+
+            fc_feature_extractions[i] = fc_feature_extractions[i].cpu().numpy()
+
+    return {'labels truth' : labels_truth, 
+            'labels pred' : labels_pred,
+            'image paths' : img_paths,
+            'feature extractions' : fc_feature_extractions}
