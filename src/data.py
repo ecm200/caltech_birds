4c9861970dc675067ccd8f82cde46195f363eaed
Ed Morris <ed.morris@malvernpanalytical.com>
2020-06-23 13:06:22 +0000

    Updates to feature maps extraction, all models completed
    Feature map extraction notebooks completed for all models trained.
    Placed universal functions into external module files.

---
+++ src/data.py
@@ -0,0 +1,18 @@
+from torchvision import datasets
+
+
+# https://gist.github.com/andrewjong/6b02ff237533b3b2c554701fb53d5c4d
+class ImageFolderWithPaths(datasets.ImageFolder):
+    """Custom dataset that includes image file paths. Extends
+    torchvision.datasets.ImageFolder
+    """
+
+    # override the __getitem__ method. this is the method that dataloader calls
+    def __getitem__(self, index):
+        # this is what ImageFolder normally returns 
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+        # the image file path
+        path = self.imgs[index][0]
+        # make a new tuple that includes original and the path
+        tuple_with_path = (original_tuple + (path,))
+        return tuple_with_path
---
+++ src/transforms.py
@@ -0,0 +1,22 @@
+import torchvision.transforms as transforms
+
+# Default training and testing augmentations transforms
+def makeDefaultTransforms(img_crop_size=224, img_resize=256):
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(img_crop_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(img_resize),
+            transforms.CenterCrop(img_crop_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    return data_transforms
