b82de68c1fb9e991063de69f957d6500ffc2f6f1
Ed Morris <ecm200@gmail.com>
2020-07-06 23:47:22 +0100

    Updates to example notebooks
    Dataset visualisation notebook started.

--- cub_tools/cub_tools/transforms.py
+++ cub_tools/cub_tools/transforms.py
@@ -20,3 +20,23 @@ def makeDefaultTransforms(img_crop_size=224, img_resize=256):
     }
 
     return data_transforms
+
+def resizeCropTransforms(img_crop_size=224, img_resize=256):
+    # Data augmentation and normalization for training
+    # Just normalization for validation
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize(img_resize),
+            transforms.CenterCrop(img_crop_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(img_resize),
+            transforms.CenterCrop(img_crop_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    return data_transforms
