321b4d6d737d4800ea74454051cc9eb76267a83a
Ed Morris <ed.morris@malvernpanalytical.com>
2020-07-06 16:11:47 +0000

    Created python package for src directory, renamed to cub_tools
    Created a python installable package that is now called cub_tools.
    All source code that is in functions to be written to this library.
    NOTE: All source code needs to be updated.

---
+++ cub_tools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cub_tools',
+      version='0.1',
+      description='Caltech UCSD Birds Database Tools for PyTorch Image Classification',
+      url='https://github.com/ecm200/caltech_birds',
+      author='Ed Morris',
+      author_email='ecm200@gmail.com',
+      license='MIT',
+      packages=['cub_tools'],
+      zip_safe=False)
