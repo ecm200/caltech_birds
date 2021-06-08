fe8ac90a5d654a06d80c122c425f23d0b8d706b9
ecm200 <ecm200@gmail.com>
2021-06-08 13:42:17 +0000

    Changed Task creation behaviour. The default task creation behaviour is now to queue for remote execution Input args can control whether this happens.

--- scripts/train_clearml_pytorch_ignite_caltech_birds.py
+++ scripts/train_clearml_pytorch_ignite_caltech_birds.py
@@ -21,39 +21,57 @@ parser.add_argument(
     '--clearml-project',
     dest='clearml_project',
     type=str,
-    help='Name of the ClearML project that you want the experiment to be logged in.', 
+    help='Name of the ClearML project that you want the experiment to be logged in. [Caltech Birds/Training]', 
     default='Caltech Birds/Training')
 
 parser.add_argument(
     '--clearml-dataset-project',
     dest='clearml_dataset_project',
     type=str,
-    help='Name of the ClearML project where the dataset for training and test is located.', 
+    help='Name of the ClearML project where the dataset for training and test is located. [Caltech Birds/Datasets]', 
     default='Caltech Birds/Datasets')
 
 parser.add_argument(
     '--clearml-dataset-train',
     dest='clearml_dataset_train',
     type=str,
-    help='Name of the ClearML training dataset.', 
+    help='Name of the ClearML training dataset. [cub200_2011_train_dataset]', 
     default='cub200_2011_train_dataset')
 
 parser.add_argument(
     '--clearml-dataset-test',
     dest='clearml_dataset_test',
     type=str,
-    help='Name of the ClearML testing dataset.', 
+    help='Name of the ClearML testing dataset. [cub200_2011_test_dataset]', 
     default='cub200_2011_test_dataset')
 
 parser.add_argument(
     '--clearml-output-url',
     dest='clearml_output_url',
     type=str,
-    help='Location of where the output files should be stored. Default is Azure Blob Storage. Format is azure://storage_account/container', 
+    help='Location of where the output files should be stored. Default is Azure Blob Storage. Format is azure://storage_account/container [azure://clearmllibrary/artefacts]', 
     default='azure://clearmllibrary/artefacts')
+
+parser.add_argument(
+        '--clearml-task-clone',
+        dest='clearml_task_clone',
+        action='store_true',
+        help='Create a clone of the task to be run on the remote resource, rather than running this experiment. [False]',
+        default=False
+    )
+parser.add_argument(
+    '--clearml-queue',
+    dest='clearml_queue',
+    type=str,
+    help='Name of the ClearML-Server queue that the task will be enqueded with for remote execution. Use "none" for no queuing of job. Default [gpu]', 
+    default='gpu')
 
 parser.print_help()
 args = parser.parse_args()
+
+# If no queue provided by specifying 'none', then make it None
+if args.clearml_queue.lower() == 'none':
+    args.clearml_queue = None
 
 if args.run_local:
     print('[INFO] Running the job locally and logging to ClearML-Server')
@@ -76,7 +94,7 @@ Task.add_requirements('git+https://github.com/rwightman/pytorch-image-models.git
 # Now connect the script to ClearML Server as an experiment.
 task = Task.init(
     project_name=args.clearml_project,
-    task_name='[Network: '+cfg.MODEL.MODEL_NAME+', Library: '+cfg.MODEL.MODEL_LIBRARY+'] Ignite Train PyTorch CNN on CUB200', 
+    task_name='TRAIN [Network: '+cfg.MODEL.MODEL_NAME+', Library: '+cfg.MODEL.MODEL_LIBRARY+'] Ignite Train PyTorch CNN on CUB200', 
     task_type=Task.TaskTypes.training,
     output_uri=args.clearml_output_url
     )
@@ -97,8 +115,12 @@ for key in params:
 # If True, this will create an experiment argument on the ClearML Server and terminate running locally.
 # If False, the model will train locally, and logging and artefacts will be capture as normal.
 if not args.run_local:
-    # Execute task remotely
-    task.execute_remotely()
+
+    # Execute task remotely, control whether task is cloned, or queued.
+    task.execute_remotely(
+        queue_name=args.clearml_queue,
+        clone=args.clearml_task_clone,
+        exit_process=True)
 
     # Get the dataset from the clearml-server and cache locally.
     print('[INFO] Getting a local copy of the CUB200 birds datasets')
