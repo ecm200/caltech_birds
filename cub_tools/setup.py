6245737af23e62e08ed9db06b04704217db4f36d
Dr Ed Morris <34489160+ecm200@users.noreply.github.com>
2021-06-29 17:00:00 +0100

    Bump release version for fix to v2.2.1
    Bumped version to v2.2.1 due to trainer.py bug fix

--- cub_tools/setup.py
+++ cub_tools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cub_tools',
-      version='2.2.0',
+      version='2.2.1',
       description='Caltech UCSD Birds Database Tools for PyTorch Image Classification',
       url='https://github.com/ecm200/caltech_birds',
       author='Ed Morris',
