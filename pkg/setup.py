50f4d95a6743e958fffd4fe790b50d9e7e63b521
Dr Ed Morris <34489160+ecm200@users.noreply.github.com>
2021-07-26 17:03:09 +0100

    Changing setup.py release version ready for tagging
    Updating setup.py so it is correctly tagged for release.

--- pkg/setup.py
+++ pkg/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cub_tools',
-      version='2.2.1a',
+      version='2.2.3',
       description='Caltech UCSD Birds Database Tools for PyTorch Image Classification',
       url='https://github.com/ecm200/caltech_birds',
       author='Ed Morris',
