4f6fa255a6cb4a5c6e08a7ff6822686ecdf687f4
ecm200 <ecm200@gmail.com>
2021-08-10 13:17:28 +0000

    Removed default config file specification in args Allowed no config file to be specified on execution. Removed the overwriting with a default name. This should allow configuration to be applied using cmd line args only.

--- pkg/cub_tools/args.py
+++ pkg/cub_tools/args.py
@@ -1,18 +1,15 @@
 import argparse
 
 def get_parser(config_file=None):
-
-    if config_file is None:
-        config_file = 'scripts/configs/pytorchcv/efficientnet_b0_config.yaml'
-
     parser = argparse.ArgumentParser(description='PyTorch Image Classification Trainer - Ed Morris (c) 2021')
 
     # Get the config file for the model
     parser.add_argument(
         '--config', 
         metavar="FILE", 
-        help='Path and name of configuration file for training. Should be a .yaml file.', 
-        default=config_file)
+        help='Path and name of configuration file for training. Should be a .yaml file.',
+        required=False, 
+        default='')
 
     parser.add_argument(
         '--run_local',
