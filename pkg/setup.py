17e69637a7cdbdef18bf5bae57d4496e803d9442
ecm200 <ecm200@gmail.com>
2021-08-11 15:43:19 +0000

    Fixes to PYPI pipeline and issue with package Package didn't seem to publish properly last time.

--- pkg/setup.py
+++ pkg/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='cub_tools',
-      version='2.5.4',
+      version='2.5.5',
       description='Caltech UCSD Birds Database Tools for PyTorch Image Classification',
       url='https://github.com/ecm200/caltech_birds',
       author='Ed Morris',
